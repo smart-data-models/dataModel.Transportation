04a43530929c7e3ea4694d3cfe71ac42cdcd42e3
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:51:13 +0100

    test of SQL export

---
+++ AnonymousCommuterId/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model AnonymousCommuterId of the subject dataModel.Transportation for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE AnonymousCommuterId_type AS ENUM ('AnonymousCommuterId');
+CREATE TABLE AnonymousCommuterId (address json, algorithm text, alternateName text, anonymizedId text, areaServed text, dataProvider text, date timestamp, dateCreated timestamp, dateModified timestamp, description text, dest text, id text, location json, name text, orig text, owner json, seeAlso json, source text, type AnonymousCommuterId_type);
