e46771fc89b8d868ed8fb1676aff00529761af0d
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:30:08 +0200

    test of SQL export

--- VehicleFault/schema.sql
+++ VehicleFault/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model VehicleFault of the subject dataModel.Transportation for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model VehicleFault of the subject dataModel.Transportation for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VehicleFault_type AS ENUM ('VehicleFault');
-CREATE TABLE VehicleFault (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, model text, name text, owner json, seeAlso json, source text, type VehicleFault_type, vehicle json, vehicleType text);
+CREATE TABLE VehicleFault (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, model TEXT, name TEXT, owner JSON, source TEXT, type VehicleFault_type, vehicleType TEXT);
