117438a4b061adaff7861bd7f4310ce3eb3ef500
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:29:19 +0200

    test of SQL export

--- BikeLane/schema.sql
+++ BikeLane/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model BikeLane of the subject dataModel.Transportation for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model BikeLane of the subject dataModel.Transportation for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE BikeLane_type AS ENUM ('BikeLane');
-CREATE TABLE BikeLane (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, dateObserved timestamp, description text, id text, laneLength text, laneOccupancy text, laneWidth text, location json, name text, owner json, seeAlso json, source text, type BikeLane_type);
+CREATE TABLE BikeLane (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, dateObserved TIMESTAMP, description TEXT, laneLength NUMERIC, laneOccupancy NUMERIC, laneWidth NUMERIC, name TEXT, owner JSON, source TEXT, type BikeLane_type);
