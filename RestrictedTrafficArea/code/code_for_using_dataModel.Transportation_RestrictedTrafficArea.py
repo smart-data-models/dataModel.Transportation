193b9af00be6e1925273d06177229edc044134b6
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:20:51 +0200

    created example of code

--- RestrictedTrafficArea/code/code_for_using_dataModel.Transportation_RestrictedTrafficArea.py
+++ RestrictedTrafficArea/code/code_for_using_dataModel.Transportation_RestrictedTrafficArea.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "RestrictedTrafficArea"
 subject = "dataModel.Transportation"
-category = {'type': 'Property', 'value': ['onlyPedestrian']}
+category = ['onlyPedestrian']
 attribute = "category"
 value = category
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-notAllowedVehicleType = {'type': 'Property', 'value': ['anyVehicle']}
+notAllowedVehicleType = ['anyVehicle']
 attribute = "notAllowedVehicleType"
 value = notAllowedVehicleType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-regulation = "{'type': 'Property', 'value': 'Decree:54785/2004, Deliberation:425/2004'}"
+regulation = "Decree:54785/2004, Deliberation:425/2004"
 attribute = "regulation"
 value = regulation
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-validityEndDate = "{'type': 'Property', 'value': '2049-12-31T23:00:00.00Z'}"
+validityEndDate = "2049-12-31T23:00:00.00Z"
 attribute = "validityEndDate"
 value = validityEndDate
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
