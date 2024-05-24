747c93446e14f81fadd83c24e5b3b5e1442e10f0
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:21:15 +0200

    created example of code

--- TrafficFlowObserved/code/code_for_using_dataModel.Transportation_TrafficFlowObserved.py
+++ TrafficFlowObserved/code/code_for_using_dataModel.Transportation_TrafficFlowObserved.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "TrafficFlowObserved"
 subject = "dataModel.Transportation"
-averageHeadwayTime = {'type': 'Property', 'value': 0.5}
+averageHeadwayTime = 0.5
 attribute = "averageHeadwayTime"
 value = averageHeadwayTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-averageVehicleLength = {'type': 'Property', 'value': 9.87}
+averageVehicleLength = 9.87
 attribute = "averageVehicleLength"
 value = averageVehicleLength
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-averageVehicleSpeed = {'type': 'Property', 'value': 52.6}
+averageVehicleSpeed = 52.6
 attribute = "averageVehicleSpeed"
 value = averageVehicleSpeed
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateObserved = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2016-12-07T11:10:00'}}"
+dateObserved = "2016-12-07T11:10:00/2016-12-07T11:15:00"
 attribute = "dateObserved"
 value = dateObserved
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
