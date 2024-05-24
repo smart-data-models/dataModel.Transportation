65ca2fe6012dcbef065b772319f8536479aa503f
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:20:55 +0200

    created example of code

--- RestrictionException/code/code_for_using_dataModel.Transportation_RestrictionException.py
+++ RestrictionException/code/code_for_using_dataModel.Transportation_RestrictionException.py
@@ -24,33 +24,38 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "RestrictionException"
 subject = "dataModel.Transportation"
-allowedVehicleType = {'type': 'array', 'value': ['dieselCarEuro6', 'petrolCarEuro6']}
+allowedVehicleType = ['dieselCarEuro6', 'petrolCarEuro6']
 attribute = "allowedVehicleType"
 value = allowedVehicleType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-exceptionValidityHours = "{'type': 'string', 'value': 'Tu,Th 16:00-20:00'}"
+exceptionValidityHours = "Tu,Th 16:00-20:00"
 attribute = "exceptionValidityHours"
 value = exceptionValidityHours
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refVehicleModel = {'type': 'array', 'value': ['vehicle:VehicleModel:modelName-1']}
-attribute = "refVehicleModel"
-value = refVehicleModel
+refRestrictedTrafficArea = "urn:ngsi-ld:RestrictedTrafficArea:Milan:RestrictedTrafficAreas:GeoJson:ds51-1"
+attribute = "refRestrictedTrafficArea"
+value = refRestrictedTrafficArea
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refRestrictedTrafficArea = "{'type': 'string', 'value': 'urn:ngsi-ld:RestrictedTrafficArea:Milan:RestrictedTrafficAreas:GeoJson:ds51-1'}"
-attribute = "refRestrictedTrafficArea"
-value = refRestrictedTrafficArea
+refVehicleModel = ['vehicle:VehicleModel:modelName-1']
+attribute = "refVehicleModel"
+value = refVehicleModel
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
